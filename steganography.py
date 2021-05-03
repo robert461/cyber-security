fb8a6d498c105845f0d538db5678ab61d86a6f71
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2021-05-03 20:18:53 +0200

    Add test for reading/writing of WAVFiles

---
+++ steganography.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import argparse
+
+from wav_steganography.wav_file import WAVFile
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Encode message into a WAV file.")
+    parser.add_argument("input", type=str, help="input file path")
+    parser.add_argument("-o", "--output", type=str, help="output file path")
+    parser.add_argument("-e", "--encode", type=str, help="encode message into wav file")
+    parser.add_argument("-d", "--decode", type=bool, help="decode message from wav file")
+    parser.add_argument("-p", "--password", type=bool, help="encrypt message with a password (will ask for password)")
+    args = parser.parse_args()
+    wav_file = WAVFile(args.input)
+    wav_file.write("test.wav")
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_wav_file.py
+++ tests/test_wav_file.py
@@ -1,3 +1,4 @@
+import hashlib
 from pathlib import Path
 
 from wav_steganography.wav_file import WAVFile
@@ -11,3 +12,17 @@ def test_loading_and_plotting_wav_file():
         plots_path = audio_path / 'plots'
         plots_path.mkdir(exist_ok=True)
         file.plot(to_s=None, filename=plots_path / audio_file.name.replace(".wav", ".png"))
+
+
+def test_loading_and_writing_wav_file():
+    audio_path = Path("audio")
+    for audio_file in audio_path.glob("*.wav"):
+        md5checksum = hashlib.md5(open(audio_file, 'rb').read()).hexdigest()
+        print(f"Loading audio file {audio_file}")
+        file = WAVFile(audio_file)
+        written_path = audio_path / 'copied'
+        written_path.mkdir(exist_ok=True)
+        copied_file_path = written_path / audio_file.name
+        file.write(copied_file_path)
+        copied_md5checksum = hashlib.md5(open(copied_file_path, 'rb').read()).hexdigest()
+        assert md5checksum == copied_md5checksum, "Checksums mismatch!"
