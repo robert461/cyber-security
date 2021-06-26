7e795fb35020596eff4e88041cf3b44fa8a1cad8
lg218 <lg218@uni-rostock.de>
2021-06-26 14:48:08 +0200

    Now creating randomized test case for all given samples

--- evaluation/degradation_eval.py
+++ evaluation/degradation_eval.py
@@ -1,7 +1,8 @@
 import pathlib
 import random
 import csv
-import simpleaudio as sa
+import os
+from simpleaudio import WaveObject
 from datetime import datetime
 
 
@@ -12,47 +13,64 @@ EVAL_REPORT_FILE_STRING = 'eval_report_'
 
 
 def process_examples():
-    print(audio_file_path)
-    for test_set in audio_file_path.glob('*'):
-        for audio_example in test_set.glob('*'):
-            process_example_pair(audio_example)
+    for one_min_sample in unmodified_audio_files_path.glob('*'):
+        audio_sample_list = select_rand_test_variant(one_min_sample)
+        process_example_pair(audio_sample_list, one_min_sample.name)
 
 
-def process_example_pair(audio_example):
+def select_rand_test_variant(one_min_sample):
     audio_sample_list = []
-    # add the modified file to the sample list
-    audio_sample_list.append((audio_example, True))
+    test_variant = random.randint(0, 19)
 
-    # get the corresponding unmodified file name to the modified file
-    one_min_file_name = str(audio_example.name).replace('modified_', '')
-    print(f'\nNow evaluating examples of {one_min_file_name}')
-    # get the file path of the one minute file
-    one_min_file_path = unmodified_audio_files_path.joinpath(one_min_file_name)
-    print(one_min_file_path)
-    # add the unmodified file to the sample list
-    audio_sample_list.append((one_min_file_path, False))
+    # unmodified/unmodified
+    if test_variant == 0:
+        for i in range(2):
+            audio_sample_list.append((one_min_sample, 0))
 
+    # modified/modified
+    elif test_variant == 1:
+        for i in range(2):
+            degr_directory = random.choice(os.listdir(audio_file_path))
+            sample = get_audio_file_by_example_name(one_min_sample.name, degr_directory)
+            audio_sample_list.append((sample, sample.parent.name.replace('test_', '')))
+
+    # unmodified/modified
+    else:
+        audio_sample_list.append((one_min_sample, 0))
+        degr_directory = random.choice(os.listdir(audio_file_path))
+        sample = get_audio_file_by_example_name(one_min_sample.name, degr_directory)
+        audio_sample_list.append((sample, sample.parent.name.replace('test_', '')))
+    return audio_sample_list
+
+
+def get_audio_file_by_example_name(name, directory):
+    directory = audio_file_path.joinpath(directory)
+    for file in directory.glob('*'):
+        if name in file.name:
+            return file
+
+
+def process_example_pair(audio_sample_list, example_name):
     # randomize item order in list
     randomized_sample_list = random.sample(audio_sample_list, len(audio_sample_list))
-    example_name = f'{audio_example.parent.name}_{one_min_file_name}'
-    print(example_name)
+    print(f'Testing sample {example_name}')
     play_sounds(randomized_sample_list, example_name)
 
 
 def play_sounds(randomized_sample_list, example_name):
     for iteration, (sample, modified) in enumerate(randomized_sample_list):
-        print(f'Now playing sample {iteration + 1}')
-        wave_obj = sa.WaveObject.from_wave_file(str(sample))
+        print(f'Playing sample {iteration + 1}')
+        wave_obj = WaveObject.from_wave_file(str(sample))
         play_obj = wave_obj.play()
         while play_obj.is_playing():
-            if input('Press \'s\' to end playback and go to next file') == 's':
+            if input('Type \'s\' to end playback and go to next file') == 's':
                 play_obj.stop()
                 play_obj.wait_done()
     process_user_evaluation(randomized_sample_list, example_name)
 
 
 def process_user_evaluation(randomized_sample_list, example_name):
-    choice = input('Which audio file shows degradation? File 1(1), file 2(2), both(b) or none(n)? If you want to '
+    choice = input('Which audio file showed degradation? File 1(1), file 2(2), both(b) or none(n)? If you want to '
                    'repeat the audio playback press (r)')
     if choice == '1':
         append_eval_report(randomized_sample_list, "1", example_name)
